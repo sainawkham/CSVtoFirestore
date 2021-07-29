a9866779de4ae8b78eb3e006b0e08aa420cbf093
sainawkham <sainawkham@gmail.com>
2021-07-29 11:10:27 +0630

    add directive drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DropzoneDirective } from './dropzone.directive';
 
 @NgModule({
   imports:      [ AngularFireModule.initializeApp(environment.firebaseConfig),
                   AngularFirestoreModule,
                   BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, DropzoneDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
