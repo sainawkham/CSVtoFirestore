62bbcdcba6265838e98896b60d2d1e589afbeb61
sainawkham <sainawkham@gmail.com>
2021-07-29 10:44:50 +0630

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ AngularFireModule.initializeApp(environment.firebaseConfig),
+                  AngularFirestoreModule,
+                  BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDLebgFh-6xAE7syhogFIGfC3fzYGQdi-4",
+    authDomain: "appointment-ba979.firebaseapp.com",
+    projectId: "appointment-ba979",
+    storageBucket: "appointment-ba979.appspot.com",
+    messagingSenderId: "320829136070",
+    appId: "1:320829136070:web:939fcf8bc2d36fc28b8bc7",
+    measurementId: "G-1JHK0F6BY5"
+  }
+};
