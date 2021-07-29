374e580e3ef728415febb34864f9b8cb84cda20f
sainawkham <sainawkham@gmail.com>
2021-07-29 11:29:52 +0630

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
+
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DropzoneDirective } from './dropzone.directive';
 import { UseruploadComponent } from './userupload/userupload.component';
+import { Services/csv2fireService } from './services/csv2fire.service';
 
 @NgModule({
   imports:      [ AngularFireModule.initializeApp(environment.firebaseConfig),
                   AngularFirestoreModule,
                   BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, DropzoneDirective, UseruploadComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [Services/csv2fireService]
 })
 export class AppModule { }
